d144f211f81f204f25adc55ee18ba0ababea6af3
Rinto Jose <rinto.jose@tcs.com>
2017-07-17 12:03:09 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+
+import './style/app.scss'
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import { App } from './app'
+import { INITIAL_STATE } from './state'
+import { initialize } from 'statex'
+
+initialize(INITIAL_STATE, {
+  hotLoad: process.env.NODE_ENV !== 'production',
+  domain: 'jogging-tracker'
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
