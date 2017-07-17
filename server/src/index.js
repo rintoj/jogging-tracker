0547e939250b120fd38f2c29abe9cb061607392f
Rinto Jose <rinto.jose@tcs.com>
2017-07-17 14:11:35 +0530

    add build, configure production and development scripts

--- server/src/index.js
+++ server/src/index.js
@@ -5,4 +5,6 @@ var isDev = process.env.NODE_ENV === 'development'
 // configure the api
 mongoRestifier(isDev ? 'server/conf/api-dev.conf.json' : 'server/conf/api.conf.json')
   .registerModel(require('./model/todo'))
-  .startup();
+  .startup((app) => {
+    app.use(require('serve-static')(__dirname + '/../../ui/dist'));
+  });
