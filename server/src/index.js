3869b319bae9cd5bdd6a84e1e2d3c0077db19fe1
Rinto Jose <rinto.jose@tcs.com>
2017-07-17 14:19:48 +0530

    fix - use cloud port if available

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,10 @@ var mongoRestifier = require('mongo-restifier');
 var isDev = process.env.NODE_ENV === 'development'
 
 // configure the api
-mongoRestifier(isDev ? 'server/conf/api-dev.conf.json' : 'server/conf/api.conf.json')
+mongoRestifier(isDev ? 'server/conf/api-dev.conf.json' : 'server/conf/api.conf.json', (properties) => {
+    properties.api.port = process.env.PORT || 5000
+    return properties
+  })
   .registerModel(require('./model/todo'))
   .startup((app) => {
     app.use(require('serve-static')(__dirname + '/../../ui/dist'));
