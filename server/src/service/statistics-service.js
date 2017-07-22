310692b7d1ae550a0f2b715451bfbefcde8468e8
Rinto Jose <rinto.jose@tcs.com>
2017-07-22 10:50:59 +0530

    update static service: add yearly, monthly and weekly stastics

--- server/src/service/statistics-service.js
+++ server/src/service/statistics-service.js
@@ -30,13 +30,61 @@ function getStatistics(user) {
     }))
 }
 
+function getWeek(date) {
+  var janFirst = new Date(date.getFullYear(), 0, 1);
+  return Math.ceil((((date - janFirst) / 86400000) + janFirst.getDay()) / 7);
+}
+
+function toMinutes(time) {
+  if (time == undefined || !(time instanceof Array) || time.length !== 2) return 0
+  return time[0] * 60 + time[1]
+}
+
+function toTime(minutes) {
+  return [Math.floor(minutes / 60), minutes % 60]
+}
+
+function measure(statistics, id, value, entry) {
+  const type = `${id}.${value}`
+  const s = (statistics[type] = statistics[type] || {})
+  s[id] = value
+  if (s.slowest == undefined || s.slowest > entry.averageSpeed) s.slowest = entry.averageSpeed
+  if (s.fastest == undefined || s.fastest < entry.averageSpeed) s.fastest = entry.averageSpeed
+  if (s.shortestDistance == undefined || s.shortestDistance > entry.distance) s.shortestDistance = entry.distance
+  if (s.longestDistance == undefined || s.longestDistance < entry.distance) s.longestDistance = entry.distance
+  s.distance = (s.distance == undefined ? 0 : s.distance) + entry.distance
+  s.time = toTime((toMinutes(s.time) || 0) + toMinutes(entry.time))
+  // if (id === 'year' || id === 'overall') {
+  //   CONSOLE.LOG(TYPE, S.TIME, TOMINUTES(ENTRY.TIME))
+  // }
+}
+
 router.get('/statistics', function(request, response) {
+  getStatistics(request.user.userId).then(data => {
+    const statistics = {}
+    let fastest, slowest, month, year, week
+    data.forEach((entry, index) => {
+      const date = new Date(entry.date)
 
-  const userId = request.user.userId
+      year = date.getFullYear()
+      month = date.getMonth()
+      week = getWeek(date)
 
-  getStatistics(userId).then(data => {
-    console.log(data)
-    send(response, data, 500)
+      measure(statistics, 'year', year, entry)
+      measure(statistics, 'month', month, entry)
+      measure(statistics, 'week', week, entry)
+      measure(statistics, 'overall', 'all', entry)
+    })
+
+    const output = Object.keys(statistics).reduce((a, i) => {
+      if (/^year/.test(i))(a.yearly = a.yearly || []).push(statistics[i])
+      if (/^month/.test(i))(a.monthly = a.monthly || []).push(statistics[i])
+      if (/^week/.test(i))(a.weekly = a.weekly || []).push(statistics[i])
+      if (/^overall/.test(i))(a.overall = a.overall || []).push(statistics[i])
+      return a
+    }, {})
+
+    send(response, output)
   })
 
 })
