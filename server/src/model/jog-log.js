e73a10fae12ed7c94519926913f90972a9650e3e
Rinto Jose <rinto.jose@tcs.com>
2017-07-22 10:50:37 +0530

    add average speed to jog log sevice

--- server/src/model/jog-log.js
+++ server/src/model/jog-log.js
@@ -26,6 +26,16 @@ module.exports = {
 
   userSpace: {
     field: 'user'
+  },
+
+  configure: function() {
+
+    // calculate averageSpeed
+    this.modelSchema.pre('save', function(next) {
+      let minutes = this.time == undefined || !(this.time instanceof Array) || this.time.length != 2 ? 0 : (this.time[0] * 60 + this.time[1])
+      this.averageSpeed = minutes === 0 ? 0 : parseFloat(parseFloat(`${this.distance / minutes * 60}`).toFixed(2))
+      next()
+    })
   }
 
 }
