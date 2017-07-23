fdd5019fc186d96c2a47110a0030b46707042b23
Rinto Jose <rinto.jose@tcs.com>
2017-07-23 23:51:29 +0530

    update revoke

--- server/unit/revoke-service.spec.js
+++ server/unit/revoke-service.spec.js
@@ -3,26 +3,25 @@ const chai = require('./chai')
 const baseUrl = 'http://localhost:5000'
 const url = '/api/oauth2/revoke'
 
-xdescribe('/api/oauth2/revoke', () => {
+describe('/api/oauth2/revoke', () => {
 
   before(async() => chai.setup(baseUrl))
+  after(async() => chai.setup(baseUrl))
 
   it('should be accessible', async() => {
-    const res = await chai.request(baseUrl).options(url)
+    const res = await chai.options(baseUrl, url)
     res.should.have.status(200)
     res.text.should.equal('POST')
   })
 
   it('should not revoke a token if incorrect credentials are passed', async() => {
-    chai.request(baseUrl).post(url).end((err, res) => {
+    chai.post(baseUrl, url, 'Basic').end((err, res) => {
       res.should.have.status(400)
     })
   })
 
   it('should revoke an issued token if correct credentials are passed', async() => {
-    const res = await chai.request(baseUrl).post(url)
-      .set('Content-Type', 'application/x-www-form-urlencoded')
-      .set('Authorization', chai.ACCESS_TOKEN)
+    const res = await chai.post(baseUrl, url)
     res.should.have.status(200)
   })
 })
