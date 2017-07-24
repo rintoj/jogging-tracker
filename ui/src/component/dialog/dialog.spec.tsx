8d28f620c583a55af8294bed5d4614b01d988a5f
Rinto Jose <rinto.jose@tcs.com>
2017-07-24 21:54:05 +0530

    add test case for dialog

---
+++ ui/src/component/dialog/dialog.spec.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+import { chai, expect } from '../../test'
+
+import { Dialog } from './dialog'
+import { shallow } from 'enzyme'
+
+describe('<Dialog/>', () => {
+
+  it('renders a dialog', () => {
+    const wrapper = shallow(<Dialog>Sample Content</Dialog>)
+    expect(wrapper.text()).to.contain('Sample Content')
+  })
+
+  it('should emit onClose event when dialog is closed', () => {
+    const onClose = chai.spy()
+    const wrapper = shallow(<Dialog onClose={event => onClose()}>Sample Text</Dialog>)
+    wrapper.find('.backdrop-node').simulate('click')
+    onClose.should.have.been.called()
+  })
+
+  it('should render without errors if onClose is not defined', () => {
+    const onClose = chai.spy()
+    const wrapper = shallow(<Dialog >Sample Text</Dialog>)
+    wrapper.find('.backdrop-node').simulate('click')
+    onClose.should.have.not.been.called()
+  })
+
+})
--- ui/src/component/dialog/dialog.tsx
+++ ui/src/component/dialog/dialog.tsx
@@ -10,7 +10,7 @@ export class Dialog extends React.Component<Props, State> {
 
   render() {
     return <div className="absolute absolute--fill flex items-center justify-center">
-      <div className="title o-50 absolute absolute--fill" onClick={event => this.close()}></div>
+      <div className="title o-50 absolute absolute--fill backdrop-node" onClick={event => this.close()}></div>
       <div className={`flex flex-column justify-center mv4 ${this.props.color || 'white'} shadow-2 br1 z-1`}>
         {this.props.children}
       </div>
