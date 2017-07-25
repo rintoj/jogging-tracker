238d0ab126a1435c0ecf10f229d2ddb1a720a3b6
Rinto Jose <rinto.jose@tcs.com>
2017-07-26 01:23:52 +0530

    remove util service - not being used

--- ui/src/service/index.ts
+++ ui/src/service/index.ts
@@ -1,11 +1,9 @@
 import { jogLogService } from './jog-log.service'
 import { statisticsService } from './statistics-service'
 import { userService } from './user-service'
-import { utilService } from './util-service'
 
 export const services = {
   jogLogService,
   statisticsService,
-  userService,
-  utilService
+  userService
 }
--- ui/src/service/util-service.ts
+++
@@ -1,8 +0,0 @@
-
-export class UtilService {
-  generateId() {
-    return btoa(Math.random().toString().substr(-6))
-  }
-}
-
-export const utilService = new UtilService()
