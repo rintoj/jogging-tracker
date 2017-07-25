6e36e98ed6717a9e9063b4b21432ebc054b22629
Rinto Jose <rinto.jose@tcs.com>
2017-07-26 02:24:11 +0530

    add statistics service

---
+++ ui/src/service/statistics-service.spec.ts
@@ -0,0 +1,19 @@
+import 'jsdom-global/register'
+
+import { api } from './api'
+import { chai } from '../test/index'
+import { statisticsService } from './statistics-service'
+
+describe('user-service', () => {
+
+  before(() => chai.simulateBrowser())
+
+  it('should fetch statistics for the given user', () => {
+    const spy = chai.spy(() => new Promise((resolve) => resolve({
+      data: {}
+    })))
+    api.get = spy
+    statisticsService.fetch('user1')
+    spy.should.have.been.called.with('/statistics?user=user1')
+  })
+})
--- ui/src/service/statistics-service.ts
+++ ui/src/service/statistics-service.ts
@@ -1,10 +1,11 @@
+import { Statistics } from '../state/statistics'
 import { api } from './api'
 
 export class StatisticsService {
 
   readonly url = '/statistics'
 
-  fetch(user: string) {
+  fetch(user: string): Promise<Statistics> {
     return api.get(`${this.url}?user=${user}`)
       .then(response => response.data)
   }
