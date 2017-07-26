444f79698a9e28a99388d73c09a0e328d9ae49c8
Rinto Jose <rinto.jose@tcs.com>
2017-07-26 22:14:18 +0530

    update sign in page tests

--- ui/e2e/signin-page.test.js
+++ ui/e2e/signin-page.test.js
@@ -8,41 +8,29 @@ fixture('signin')
 const idInput = Selector('#userId')
 const passwordInput = Selector('#password')
 const submitButton = Selector('button').nth(0)
+const signupButton = Selector('button').nth(1)
 
-test('sign in form validation', async t => {
-  await t
-    .click(submitButton)
-    .expect(Selector('.error-text').exists).ok('Validation failed')
+test('should show validation errors if incorrect values are entered', async t => {
+  await t.click(submitButton)
+    .expect(Selector('.error-text').nth(0).innerText).eql('Required')
+    .expect(Selector('.error-text').nth(1).innerText).eql('Required')
 })
 
-test('sign in success', async t => {
-  await t
-    .typeText(idInput, 'admin@system.com')
-    .setTestSpeed(0.1)
-    .expect(idInput.value).contains('admin@system.com')
-
-  await t
-    .typeText(passwordInput, 'admin')
-    .setTestSpeed(0.1)
-    .expect(passwordInput.value).contains('admin')
-
-  await t
-    .click(submitButton)
+test('should successfully validate user id and password and navigate to home page', async t => {
+  await t.typeText(idInput, 'admin@system.com')
+  await t.typeText(passwordInput, 'admin')
+  await t.click(submitButton)
     .expect(Selector('.menu-node').exists).ok('Login failed!')
 })
 
-test('sign in failure', async t => {
-  await t
-    .typeText(idInput, 'admin@system.com')
-    .setTestSpeed(0.1)
-    .expect(idInput.value).contains('admin@system.com')
-
-  await t
-    .typeText(passwordInput, 'invalid-pass')
-    .setTestSpeed(0.1)
-    .expect(passwordInput.value).contains('invalid-pass')
-
-  await t
-    .click(submitButton)
+test('should validate user and should not navigate to home page if fails', async t => {
+  await t.typeText(idInput, 'admin@system.com')
+  await t.typeText(passwordInput, 'invalid-pass')
+  await t.click(submitButton)
     .expect(Selector('.menu-node').exists).notOk('Login did not fail!')
+})
+
+test('should navigate to signup page if signup button is clicked', async t => {
+  await t.click(signupButton)
+    .expect(Selector('.f2.mb4').exists).ok('Navigation to signup page failed!')
 })
